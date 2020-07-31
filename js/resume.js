562f681e0ddf2363e718888aa5ecfc0a8819c0d7
Brandon Jurado <brandonjurado13@yahoo.com>
2020-07-31 08:41:10 -0500

    Don't push yet

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,13 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // On page load
+  $(document).ready(function() {
+    $('#demo-tooltip-above').jBox('Tooltip', {
+      theme: 'TooltipDark'
+    });
+  }
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
