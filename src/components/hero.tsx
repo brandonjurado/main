ebcd99699c168953ce8ea1652f0164fa56297d7e
Brandon Jurado <brandonjurado13@yahoo.com>
2020-08-26 22:40:14 -0500

    Move parallax triangle

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,7 +12,7 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
   <div>
     <Divider speed={0.2} offset={offset} factor={factor}>
       <UpDown>
-        <SVG icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="13%" />
+        <SVG icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="10%" />
         <SVG icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
         <SVG icon="box" width={6} color="icon_darker" left="60%" top="15%" />
       </UpDown>
