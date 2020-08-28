802e09a141b7bb8a56d754de26ff54b6f63d20ec
Brandon Jurado <brandonjurado13@yahoo.com>
2020-08-27 22:07:45 -0500

    Format skills list

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -36,7 +36,7 @@ const Skills = ({ offset, factor = 1 }: { offset: number; factor?: number }) => 
       </UpDownWide>
       <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="4%" top="20%" />
       <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
-      <SVG icon="box" width={6} color="icon_orange" left="10%" top="10%" />
+      <SVG icon="box" width={6} color="icon_orange" left="15%" top="10%" />
       <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
       <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
     </Divider>
